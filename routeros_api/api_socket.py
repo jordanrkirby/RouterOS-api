060e67dabd84c52d15860ddc09efcf9c78561f53
Jordan Kirby <j.kirby@node4.co.uk>
2022-07-25 14:59:42 +0100

    IPv6 Support

--- routeros_api/api_socket.py
+++ routeros_api/api_socket.py
@@ -1,3 +1,4 @@
+import ipaddress
 import socket
 import ssl
 from routeros_api import exceptions
@@ -9,7 +10,10 @@ except ImportError:
 EINTR = getattr(errno, 'EINTR', 4)
 
 def get_socket(hostname, port, use_ssl=False, ssl_verify=True, ssl_verify_hostname=True, ssl_context=None, timeout=15.0):
-    api_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if ipaddress.IPv6Address(unicode(hostname)):
+        api_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    else:
+        api_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     api_socket.settimeout(timeout)
     while True:
         try:
